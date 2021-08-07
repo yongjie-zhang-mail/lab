30f7968772af85ca03c3e09649ffa8f079eb31f5
yongjie-zhang-mail <61307704+yongjie-zhang-mail@users.noreply.github.com>
2021-08-07 10:39:44 +0800

    initial project

---
+++ src/test/java/com/yj/lab/DemoTests.java
@@ -0,0 +1,16 @@
+package com.yj.lab;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DemoTests {
+
+    public static void main(String[] args) {
+
+        log.info("message");
+
+    }
+
+
+
+}
