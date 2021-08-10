a5b64334b9349798589789108e31b790e40fbfb7
yongjie <yongjie_zhang_mail@qq.com>
2021-08-10 23:49:44 +0800

    update 122

---
+++ src/test/java/com/yj/lab/alog/dp/Dp122.java
@@ -0,0 +1,16 @@
+package com.yj.lab.alog.dp;
+
+public class Dp122 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        public static int maxProfit(int[] prices) {
+
+            return 0;
+        }
+    }
+
+}
